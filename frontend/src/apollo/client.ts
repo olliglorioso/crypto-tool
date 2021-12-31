cee44d858d5e2009a46e3f675dbe8273fe3d632f
olliglorioso <olli.glorioso@gmail.com>
2021-12-31 13:54:16 +0200

    Heroku instead.

--- frontend/src/apollo/client.ts
+++ frontend/src/apollo/client.ts
@@ -1,10 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
+const uri = process.env.NODE_ENV === "development" ? "http://localhost:4000/graphql" : "https://crypto-tool-bitcoin.herokuapp.com/graphql";
 // Initialize the apollo client.
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-        uri: "http://localhost:4000/graphql",
+        uri
     })
 });
 
